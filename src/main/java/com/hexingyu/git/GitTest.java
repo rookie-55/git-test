ff06c385a1df010058582101fd9dd1cfcab880d9
hexingyu <hexingyu@qq.com>
2022-05-29 22:50:02 +0800

    push commit

--- src/main/java/com/hexingyu/git/GitTest.java
+++ src/main/java/com/hexingyu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello4 ");
         System.out.println("master test ");
         System.out.println("hotfix test ");
+        System.out.println("push test ");
     }
 }
